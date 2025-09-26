8560b3ee8005a1040a48d7062397a3d1a033c126
Frankie Snedecor <frankie.snedecor@gmail.com>
2025-09-25 17:28:09 -0700

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,22 @@ package org.example;
 
 import net.tcgdex.sdk.TCGdex;
 import net.tcgdex.sdk.models.Card;
+import net.tcgdex.sdk.models.CardResume;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
+        //GET https://api.tcgdex.net/v2/en/cards?name=zekrom
         System.out.printf("Hello and welcome!");
+
+        TCGdex tcgdex = new TCGdex("en");
+
+
+
+
+        System.out.println(tcgdex.fetchSets());
+
     }
 }
